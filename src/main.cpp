9fed246e6a523130c60f60e04b67725d8a287986
Helbushek <gorevaya134@gmail.com>
2025-05-16 02:38:54 +0700

    Removed unneeded comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    qDebug() << " main start";
     QApplication a(argc, argv);
-    qDebug() << "   after QApplication ctor";
     MainWindow w;
-    qDebug() << "   after MainWindow ctor";
     w.show();
-    qDebug() << "   after w.show()";
     return a.exec();
 }
